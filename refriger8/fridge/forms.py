69c4c7f06049fa20f2823a09c57c030e2382d8fb
Alexander Zielonka <alexander.zie@mail.de>
2025-04-07 22:07:35 +0200

    working project implemented

---
+++ refriger8/fridge/forms.py
@@ -0,0 +1,11 @@
+# fridge/forms.py
+from django import forms
+from .models import FoodItem
+
+class FoodItemForm(forms.ModelForm):
+    class Meta:
+        model = FoodItem
+        fields = ['name', 'category', 'quantity', 'best_by']
+        widgets = {
+            'best_by': forms.DateInput(attrs={'type': 'date'}),
+        }
